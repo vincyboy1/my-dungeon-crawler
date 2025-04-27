ddfec2826af09403d7296160f2f876700e10eae2
Joe <jovanisam123@gmail.com>
2025-04-27 15:41:21 -0700

    updating main root

--- src/ReplicatedStorage/Knit/Controllers/ClassController.lua
+++ src/ReplicatedStorage/Knit/Controllers/ClassController.lua
@@ -1,9 +1,8 @@
--- src/StarterPlayer/StarterPlayerScripts/ClassController.client.lua
+-- src/ReplicatedStorage/Knit/Controllers/ClassController.lua
 local Players           = game:GetService("Players")
-local TextChatService  = game:GetService("TextChatService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local TextChatService   = game:GetService("TextChatService")
+local Knit              = require(script.Parent.Parent.Knit)
 
-local Knit = require(ReplicatedStorage.Knit.Knit)
 local ClassController = Knit.CreateController { Name = "ClassController" }
 
 function ClassController:KnitStart()
@@ -11,26 +10,17 @@ function ClassController:KnitStart()
     local ClassService = Knit.GetService("ClassService")
     local player = Players.LocalPlayer
 
-    -- Legacy chat event
     player.Chatted:Connect(function(msg)
         self:_handleChat(msg, ClassService)
     end)
 
-    -- New TextChatService event
     TextChatService.OnIncomingMessage:Connect(function(message)
-        -- message.Text is the chat string
         self:_handleChat(message.Text, ClassService)
     end)
 end
 
--- parse and dispatch
 function ClassController:_handleChat(msg, ClassService)
-    -- Pattern breakdown: 
-    --  ^           : start of the string 
-    --  /class      : literal “/class” 
-    --  %s+         : one or more spaces 
-    --  (%w+)       : capture one or more “word” chars (letters, digits, underscore)
-    local chosen = msg:match("^/class%s+(%w+)")
+    local chosen = msg:match("^/class%s+(.+)$")
     if chosen then
         print("[ClassController] Requesting class:", chosen)
         ClassService:SelectClass(chosen)
